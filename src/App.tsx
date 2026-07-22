517c326b7a00430f2f71f76767990c97a5659f6a
Vaintseishii <xanthreign.palmes-25@cpu.edu.ph>
2026-07-22 23:05:32 +0800

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="app">
       <header className="header">
+        <button className="menu-button">☰</button>
         <h1>Blackout EDC</h1>
+        <p className="cart">Cart (0)</p>
       </header>
     </div>
   );
