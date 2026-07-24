388651d423fa218d99f8927da47306d3e0306089
Vaintseishii <xanthreign.palmes-25@cpu.edu.ph>
2026-07-24 17:26:57 +0800

    add product cart hover animation

--- src/productBox.tsx
+++ src/productBox.tsx
@@ -3,18 +3,25 @@ import black_wallet from "./assets/black_wallet.png";
 
 export default function ProductBox() {
   return (
-   <div className="productCard">
-      <div className="boxSize">
-      <img
-        src={black_wallet}
-        alt="Black Wallet"
-        className="productImage"
-      />
+    <div className="productCard">
+      <div className="productContent">
+        <div className="boxSize">
+          <img
+            src={black_wallet}
+            alt="Black Wallet"
+            className="productImage"
+          />
+        </div>
+
+        <div className="productInfo">
+          <h2 className="productTitle">Black Wallet</h2>
+          <p className="productPrice">₱799</p>
+        </div>
+      </div>
+
+      <div className="productOverlay">
+        <span className="plus">+</span>
+      </div>
     </div>
-    <div className="productTitle">
-      <h1>Black wallet</h1>
-    </div>   
-   </div>
-    
   );
 }
