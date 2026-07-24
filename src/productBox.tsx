d4ae853336f9bb473390258b709368949f53a64a
Vaintseishii <xanthreign.palmes-25@cpu.edu.ph>
2026-07-24 13:06:45 +0800

    update productbox layout

--- src/productBox.tsx
+++ src/productBox.tsx
@@ -1,9 +1,20 @@
-import "./productBox.css"
+import "./productBox.css";
+import black_wallet from "./assets/black_wallet.png";
 
 export default function ProductBox() {
-   return(
-      <div className = "boxSize">
-	aaaaaa
-      </div>
-   )
+  return (
+   <div className="productCard">
+      <div className="boxSize">
+      <img
+        src={black_wallet}
+        alt="Black Wallet"
+        className="productImage"
+      />
+    </div>
+    <div className="productTitle">
+      <h1>Black wallet</h1>
+    </div>   
+   </div>
+    
+  );
 }
